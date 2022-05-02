e0b639b37a3a0d8cee5fc9dd1d2af314a713ef77
Mikhail Liakhovets <mikhail.liakhovets@arkadium.com>
2022-05-02 14:50:19 +0300

    Create project structure

---
+++ src/components/index.tsx
@@ -0,0 +1,9 @@
+export { default as About } from "./about";
+export { default as Contact } from "./contact";
+export { default as Experience } from "./experience";
+export { default as Footer } from "./footer";
+export { default as Header } from "./header";
+export { default as Nav } from "./nav";
+export { default as Portfolio } from "./portfolio";
+export { default as Services } from "./services";
+export { default as Testimonials } from "./testimonials";
