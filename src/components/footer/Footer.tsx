e6351e1050a3859bc19ef65f2cc4071bfe5d3e57
Mikhail Liakhovets <mikhail.liakhovets@arkadium.com>
2022-05-21 15:01:41 +0300

    Add footer, fix link styles

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
 import "./footer.css";
 
 const Footer = () => {
+  const permalinks = [
+    { link: "#", title: "Home" },
+    { link: "#about", title: "About" },
+    { link: "#experience", title: "Experience" },
+    { link: "#portfolio", title: "Portfolio" },
+    { link: "#contacts", title: "Contacts" },
+  ];
   return (
-    <footer>Footer</footer>
-  )
-}
+    <footer>
+      <a href="#" className="footer-logo">
+        Mikhail Liakhovets
+      </a>
+      <ul className="permalinks">
+        {permalinks.map((item) => (
+          <li key={item.title}>
+            <a href={item.link}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
